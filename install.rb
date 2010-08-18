49e35ca1ba79693276d6dc42f70cd6d086ee044b
Pavlo V. Lysov <pavlikus@gmail.com>
2010-08-18 12:41:59 +0300

    implements a simple installation routine

---
+++ install.rb
@@ -0,0 +1,9 @@
+puts "Copying files required for ClientTimezoneDetection..."
+
+["jquery-cookie.js", "time_zone_detection.js"].each do |js_file|
+	dest_file = File.join(RAILS_ROOT, "public", "javascripts", js_file)
+	src_file = File.join(File.dirname(__FILE__) , "fixtures", js_file)
+	FileUtils.cp_r(src_file, dest_file)
+end
+
+puts "ClientTimezoneDetection installed successfully"
